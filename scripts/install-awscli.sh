bc0848645f151a165edbefdb5a34b54799d1f72b
Sean Harvey <sean@storj.io>
2021-07-16 12:39:17 +1200

    ci: fix aws-cli not being installed in ci image
    first issue is that the install-awscli.sh script is not correctly
    checking for errors, so we were still seeing successful builds of
    the ci image, even when the aws-cli install was failing.
    
    secondly, the aws-cli v1 no longer installs if it detects Python 2
    installed on the system, which started occuring 15 July 2021.
    Switching to installing the v2 client works fine, as it contains
    its own interpreter and doesn't rely on installed packages.
    
    attempting to remove Python 2 or set default to Python 3 may be a
    rabbit hole, so rather than go down that path I think upgrading to
    the latest aws-cli version is better. AWS recommends everyone upgrade
    to this, and it works fine with our gateways.
    
    Change-Id: I8c6776b3e6ec29a847a66e6dff070e75852ba7e8

--- scripts/install-awscli.sh
+++ scripts/install-awscli.sh
@@ -1,12 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
 set -x
 
-mkdir -p $HOME/awscli
-pushd $HOME/awscli
+mkdir -p "$HOME/awscli"
+pushd "$HOME/awscli"
 
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-./awscli-bundle/install -b ~/bin/aws
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install -b ~/bin
 
 popd
+rm -r "$HOME/awscli"
 
 set +x
