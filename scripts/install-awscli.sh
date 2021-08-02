d662750d1328a1770f256903bb3be39a259939d6
Sean Harvey <sean@storj.io>
2021-08-02 14:02:43 +1200

    ci: remove noise in logs during awscli installation
    Change-Id: If9ed3fbf97786f8d4479074530539f0af84a227f

--- scripts/install-awscli.sh
+++ scripts/install-awscli.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
-set -x
 
 mkdir -p "$HOME/awscli"
 pushd "$HOME/awscli"
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -q awscliv2.zip
 ./aws/install -b ~/bin
 
 popd
 rm -r "$HOME/awscli"
-
-set +x
