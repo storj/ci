5286f52c805ed28226d75e9c6b2644e43959aa0d
Egon Elbre <egonelbre@gmail.com>
2022-09-08 15:56:36 +0300

    images/slim.sh: bump golangci and staticcheck
    Change-Id: I117fa73b5a12eb7902ded6b47ba0b23859f473be

--- images/slim.sh
+++ images/slim.sh
@@ -19,11 +19,11 @@ curl -sfL https://github.com/protocolbuffers/protobuf/releases/download/v3.12.3/
 apt-get -y install shellcheck
 
 # Linters, formatters, build tools
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.46.2 && \
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.47.3 && \
     go install github.com/ckaznocha/protoc-gen-lint@v0.2.4 && \
     go install github.com/nilslice/protolock/cmd/protolock@v0.15.2 && \
     go install github.com/josephspurrier/goversioninfo/cmd/goversioninfo@63e6d1acd3dd857ec6b8c54fbf52e10ce24a8786 && \
-    go install honnef.co/go/tools/cmd/staticcheck@2022.1.2 && \
+    go install honnef.co/go/tools/cmd/staticcheck@2022.1.3 && \
     # Output formatters \
     go install github.com/mfridman/tparse@36f80740879e24ba6695649290a240c5908ffcbb  && \
     go install github.com/axw/gocov/gocov@v1.0.0  && \
