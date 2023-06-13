c72982825f56a9f8f7204df9e0b47074a4018d4d
Antonio Franco <antonio@storj.io>
2023-06-13 01:33:21 -0400

    using different install mechanism.

--- images/ui.sh
+++ images/ui.sh
@@ -1,6 +1,7 @@
 set -euo pipefail
 
 # Requirements for UI tests
-npx playwright install-deps
-npx playwright install
+# npx playwright install-deps
+# npx playwright install
+npx playwright install --with-deps
 npm install -g @playwright/test
