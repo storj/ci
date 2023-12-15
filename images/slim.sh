ffd9f56f9b5a4125933231a75d9189b91b040d9a
Cameron <cameron@storj.io>
2023-12-15 11:35:13 -0500

    install npm

--- images/slim.sh
+++ images/slim.sh
@@ -22,7 +22,7 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dea
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
-DEBIAN_FRONTEND="noninteractive" apt-get install -y brotli unzip libuv1-dev libjson-c-dev nettle-dev nodejs
+DEBIAN_FRONTEND="noninteractive" apt-get install -y brotli unzip libuv1-dev libjson-c-dev nettle-dev nodejs npm
 npm install -g npm@9.6.7
 npm install -g pnpm@v8
 
