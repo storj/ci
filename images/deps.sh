7e74f6a756f9be3a5a0d606e6a2d6c956efb498e
Márton Elek <elek@apache.org>
2024-03-28 10:50:32 +0100

    images: add gcloud spanner emulator to the storjlabs/ci image
    Change-Id: Icaf8c9b56aa9c21d34ec89d0a4c066068b2f0df6

--- images/deps.sh
+++ images/deps.sh
@@ -1,3 +1,4 @@
+
 set -xeuo pipefail
 
 # CockroachDB
@@ -6,7 +7,7 @@ wget -qO- https://binaries.cockroachdb.com/cockroach-v23.2.2.linux-amd64.tgz | t
 mv cockroach-v23.2.2.linux-amd64/cockroach /usr/local/bin/
 mv cockroach-v23.2.2.linux-amd64/lib/* /usr/lib/
 
-# Postgres
+# Postgres & Redis
 
 curl -sf https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 echo "deb http://apt.postgresql.org/pub/repos/apt/ bookworm-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
@@ -21,3 +22,13 @@ echo 'host    all             all             ::0/0                   trust' >> 
 
 echo 'max_connections = 1000' >> /etc/postgresql/13/main/conf.d/connectionlimits.conf
 echo 'fsync = off' >> /etc/postgresql/13/main/conf.d/nosync.conf
+
+# Google Cloud CLI for spanner emulator
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+apt-get update && apt-get install google-cloud-cli google-cloud-cli-spanner-emulator
+
+gcloud config configurations create emulator
+gcloud config set auth/disable_credentials true
+gcloud config set project storj-build
+gcloud config set api_endpoint_overrides/spanner http://localhost:9020/
