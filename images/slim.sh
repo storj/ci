909311b13544b90f0124b1034c921aed63b26427
Egon Elbre <egonelbre@gmail.com>
2024-08-26 17:08:24 +0300

    images: install git-restore-mtime
    Change-Id: Iac94054fcea87153a60fd35f2cc5e7bf4000c290

--- images/slim.sh
+++ images/slim.sh
@@ -13,7 +13,7 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dea
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
-apt-get update && DEBIAN_FRONTEND="noninteractive" apt-get install -y brotli unzip libuv1-dev libjson-c-dev nettle-dev nodejs
+apt-get update && DEBIAN_FRONTEND="noninteractive" apt-get install -y brotli unzip libuv1-dev libjson-c-dev nettle-dev nodejs git-restore-mtime
 
 npm install -g npm@10.4.0
 npm install -g pnpm@v8
