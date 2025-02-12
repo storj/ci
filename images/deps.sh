27b5ddd21e94bb72c34ef49a825475e388a08e3a
Egon Elbre <egonelbre@gmail.com>
2025-02-12 10:58:15 +0200

    images: bump PostgreSQL to v17
    Change-Id: I6279349eefed3b4b3105e42504ae09bdbfedc1d1

--- images/deps.sh
+++ images/deps.sh
@@ -12,16 +12,16 @@ mv cockroach-v23.2.3.linux-amd64/lib/* /usr/lib/
 curl -sf https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 echo "deb http://apt.postgresql.org/pub/repos/apt/ bookworm-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
 
-apt-get update && apt-get install -y -qq postgresql-13 redis-server
+apt-get update && apt-get install -y -qq postgresql-17 redis-server
 
-rm /etc/postgresql/13/main/pg_hba.conf
-echo 'local   all             all                                     trust' >> /etc/postgresql/13/main/pg_hba.conf
-echo 'host    all             all             127.0.0.1/8             trust' >> /etc/postgresql/13/main/pg_hba.conf
-echo 'host    all             all             ::1/128                 trust' >> /etc/postgresql/13/main/pg_hba.conf
-echo 'host    all             all             ::0/0                   trust' >> /etc/postgresql/13/main/pg_hba.conf
+rm /etc/postgresql/17/main/pg_hba.conf
+echo 'local   all             all                                     trust' >> /etc/postgresql/17/main/pg_hba.conf
+echo 'host    all             all             127.0.0.1/8             trust' >> /etc/postgresql/17/main/pg_hba.conf
+echo 'host    all             all             ::1/128                 trust' >> /etc/postgresql/17/main/pg_hba.conf
+echo 'host    all             all             ::0/0                   trust' >> /etc/postgresql/17/main/pg_hba.conf
 
-echo 'max_connections = 1000' >> /etc/postgresql/13/main/conf.d/connectionlimits.conf
-echo 'fsync = off' >> /etc/postgresql/13/main/conf.d/nosync.conf
+echo 'max_connections = 1000' >> /etc/postgresql/17/main/conf.d/connectionlimits.conf
+echo 'fsync = off' >> /etc/postgresql/17/main/conf.d/nosync.conf
 
 # Google Cloud Spanner Emulator binaries
 SPANNER_VERSION=1.5.28
