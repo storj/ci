6fa09a5ab269c2eb21a89e36fb8a5ee8719a8a82
Egon Elbre <egonelbre@gmail.com>
2025-04-10 10:04:57 +0300

    images: update cloud-spanner-emulator@1.5.32
    Change-Id: I9041b56449bd9916ba67a38d365a1cf1418b9b04

--- images/deps.sh
+++ images/deps.sh
@@ -24,10 +24,10 @@ echo 'max_connections = 1000' >> /etc/postgresql/17/main/conf.d/connectionlimits
 echo 'fsync = off' >> /etc/postgresql/17/main/conf.d/nosync.conf
 
 # Google Cloud Spanner Emulator binaries
-SPANNER_VERSION=1.5.31
+SPANNER_VERSION=1.5.32
 SPANNER_ARCH=amd64
 curl -0 https://storage.googleapis.com/cloud-spanner-emulator/releases/${SPANNER_VERSION}/cloud-spanner-emulator_linux_${SPANNER_ARCH}-${SPANNER_VERSION}.tar.gz -o cloud-spanner-emulator.tar.gz
-echo "042b8a7ee8301081ec015683ce07346b2b31e68a58490d7ef287672677786a9c *cloud-spanner-emulator.tar.gz" | sha256sum --check
+echo "57665013fd63e2c959f3b2949d0ecda09dba02efd065c513d228b75a4eb3de99 *cloud-spanner-emulator.tar.gz" | sha256sum --check
 tar xvf cloud-spanner-emulator.tar.gz
 chmod u+x gateway_main emulator_main
 mv gateway_main /usr/local/bin/spanner_gateway
