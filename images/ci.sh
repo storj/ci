78be6ef9b8e921e475d8567fd59a3aff4177d67d
Egon Elbre <egonelbre@gmail.com>
2025-08-13 13:46:27 +0300

    images: bump CI to Go 1.25.0
    Bump Spanner emulator to 1.5.38
    Bump Duplicati
    Bump Zig
    
    Change-Id: Ifade1f741e24fb50e99dbff2fca250d9ea6f7a78

--- images/ci.sh
+++ images/ci.sh
@@ -8,16 +8,17 @@ apt-get install -y duplicity python3-pip python3-boto3
 apt-get install -y tox
 
 # Duplicati backup tool for S3 gateway test scenarios
-apt-get -y install mono-devel
-curl -sfL https://updates.duplicati.com/beta/duplicati_2.0.7.1-1_all.deb -o /tmp/duplicati.deb
+apt-get -y install mono-devel libicu76
+curl -sfL https://updates.duplicati.com/canary/duplicati-2.1.1.100_canary_2025-08-08-linux-x64-cli.deb -o /tmp/duplicati.deb
 apt -y install /tmp/duplicati.deb
 
 # Requirements for UI tests
-npm install @playwright/test@v1.48.2
+npm install @playwright/test@next # TODO use v1.55.0 once it's released
 npx playwright install --with-deps
 
 # Install Zig for cross-compiling
-VERSION=0.13.0
+apt-get install -y xz-utils
+VERSION=0.14.0
 wget https://ziglang.org/download/$VERSION/zig-linux-x86_64-$VERSION.tar.xz
 tar -xJf zig-linux-x86_64-$VERSION.tar.xz
 rm zig-linux-x86_64-$VERSION.tar.xz
--- images/deps.sh
+++ images/deps.sh
@@ -9,10 +9,16 @@ mv cockroach-v23.2.3.linux-amd64/lib/* /usr/lib/
 
 # Postgres & Redis
 
-curl -sf https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-echo "deb http://apt.postgresql.org/pub/repos/apt/ bookworm-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
+apt install curl ca-certificates
+install -d /usr/share/postgresql-common/pgdg
+curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
 
-apt-get update && apt-get install -y -qq postgresql-17 redis-server
+# Create the repository configuration file:
+. /etc/os-release
+sh -c "echo 'deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $VERSION_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+
+# Update the package lists:
+apt update && apt install -y -qq postgresql-17 redis-server
 
 rm /etc/postgresql/17/main/pg_hba.conf
 echo 'local   all             all                                     trust' >> /etc/postgresql/17/main/pg_hba.conf
@@ -24,10 +30,10 @@ echo 'max_connections = 1000' >> /etc/postgresql/17/main/conf.d/connectionlimits
 echo 'fsync = off' >> /etc/postgresql/17/main/conf.d/nosync.conf
 
 # Google Cloud Spanner Emulator binaries
-SPANNER_VERSION=1.5.32
+SPANNER_VERSION=1.5.38
 SPANNER_ARCH=amd64
 curl -0 https://storage.googleapis.com/cloud-spanner-emulator/releases/${SPANNER_VERSION}/cloud-spanner-emulator_linux_${SPANNER_ARCH}-${SPANNER_VERSION}.tar.gz -o cloud-spanner-emulator.tar.gz
-echo "57665013fd63e2c959f3b2949d0ecda09dba02efd065c513d228b75a4eb3de99 *cloud-spanner-emulator.tar.gz" | sha256sum --check
+echo "f1e71a5cf34acb0efef0f20dca12223477362957413ea60d6ac49a5c390c6340 *cloud-spanner-emulator.tar.gz" | sha256sum --check
 tar xvf cloud-spanner-emulator.tar.gz
 chmod u+x gateway_main emulator_main
 mv gateway_main /usr/local/bin/spanner_gateway
