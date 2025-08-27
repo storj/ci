05d3496c084fed17887ff0e4ea1d5c5372272f95
Sean Harvey <sean@storj.io>
2025-08-27 20:58:59 +1200

    images: downgrade duplicati the latest stable version that works
    Change-Id: I5a4923038a588f49c4163204e18060621b5f08e8

--- images/ci.sh
+++ images/ci.sh
@@ -3,7 +3,7 @@ set -xeuo pipefail
 # AWS CLI v2.22.35 is the latest version we support currently.
 # This should be unpegged once https://github.com/storj/gateway-st/issues/89 is solved.
 curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.22.35.zip -o awscliv2.zip
-unzip awscliv2.zip
+unzip -q awscliv2.zip
 ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli
 rm awscliv2.zip
 rm -r ./aws
@@ -16,9 +16,12 @@ apt-get install -y duplicity python3-pip python3-boto3
 apt-get install -y tox
 
 # Duplicati backup tool for S3 gateway test scenarios
-apt-get -y install mono-devel libicu76
-curl -sfL https://updates.duplicati.com/canary/duplicati-2.1.1.100_canary_2025-08-08-linux-x64-cli.deb -o /tmp/duplicati.deb
-apt -y install /tmp/duplicati.deb
+DUPLICATI_NAME=duplicati-2.1.0.5_stable_2025-03-04-linux-x64-cli
+curl https://updates.duplicati.com/stable/${DUPLICATI_NAME}.zip -o duplicati.zip
+unzip -q duplicati.zip
+rm duplicati.zip
+mv "${DUPLICATI_NAME}" /usr/local/duplicati
+ln -s /usr/local/duplicati/duplicati-cli /usr/local/bin/duplicati-cli
 
 # Requirements for UI tests
 npm install @playwright/test@next # TODO use v1.55.0 once it's released
