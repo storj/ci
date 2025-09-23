eeb508242209a5e020eaa63fecf6aa3bb5d69d7a
Egon Elbre <egonelbre@gmail.com>
2025-09-23 12:06:22 +0300

    images: bump npm@v24 and pnpm@v10
    Change-Id: If98a37a1e1ef275cf87866059c358ebe1f408f11

--- images/slim.sh
+++ images/slim.sh
@@ -10,13 +10,12 @@ apt-get update && apt-get install -y ca-certificates curl gnupg
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-NODE_MAJOR=20
+NODE_MAJOR=24
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 apt-get update && DEBIAN_FRONTEND="noninteractive" apt-get install -y brotli unzip libuv1-dev libjson-c-dev nettle-dev nodejs git-restore-mtime
 
-npm install -g npm@10.4.0
-npm install -g pnpm@v8
+npm install -g pnpm@latest-10
 
 curl -sfL https://github.com/protocolbuffers/protobuf/releases/download/v3.12.3/protoc-3.12.3-linux-x86_64.zip -o /tmp/protoc.zip && unzip /tmp/protoc.zip -d "$HOME"/protoc
 
