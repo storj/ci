aa7db9f7f4a28d5194e90707862d8efce9aa70d8
Egon Elbre <egonelbre@gmail.com>
2025-11-20 12:47:06 +0200

    images: downgrade spanner emulator to v1.5.42
    RETURNING clause is not returning the correct value that is inserted into the database.
    
    See https://github.com/GoogleCloudPlatform/cloud-spanner-emulator/issues/295
    
    Change-Id: I5a9cad8803c22e40c742cbc2d9b20a7b45107881

--- images/deps.sh
+++ images/deps.sh
@@ -30,10 +30,10 @@ echo 'max_connections = 1000' >> /etc/postgresql/17/main/conf.d/connectionlimits
 echo 'fsync = off' >> /etc/postgresql/17/main/conf.d/nosync.conf
 
 # Google Cloud Spanner Emulator binaries
-SPANNER_VERSION=1.5.44
+SPANNER_VERSION=1.5.42
 SPANNER_ARCH=amd64
 curl -0 https://storage.googleapis.com/cloud-spanner-emulator/releases/${SPANNER_VERSION}/cloud-spanner-emulator_linux_${SPANNER_ARCH}-${SPANNER_VERSION}.tar.gz -o cloud-spanner-emulator.tar.gz
-echo "b2051aac8ed6069a6e0db1cfdbbbe362661968b6b239b7f7deaee73cea36808f *cloud-spanner-emulator.tar.gz" | sha256sum --check
+echo "c7255193c2f0e6e187f18fb7120b666584059315e32bdc37e4fa90563bed6259 *cloud-spanner-emulator.tar.gz" | sha256sum --check
 tar xvf cloud-spanner-emulator.tar.gz
 chmod u+x gateway_main emulator_main
 mv gateway_main /usr/local/bin/spanner_gateway
